910b514fdefddc30d08728f80da02efa39d748a2
yuanxin <yuanxin@shhxzq.com>
2017-05-26 14:40:43 +0800

    jdk8 chapter3

--- javabasic/src/yuan/java/basic/demo/sample/jdk8/lambda/FunctionDemo.java
+++ javabasic/src/yuan/java/basic/demo/sample/jdk8/lambda/FunctionDemo.java
@@ -32,36 +32,54 @@ public class FunctionDemo
         Predicate<Integer> oddNumvers = (Integer i) -> i % 2 == 0;
         oddNumvers.test(1000);
 
-        IntToDoubleFunction intToDouble = (int i) -> {
-            return i * 2;
-        };
-        double testDouble = intToDouble.applyAsDouble(3);
-        System.out.println(testDouble);
 
+    }
+
+    public void testIntConsumer()
+    {
         IntConsumer intConsumer = (int i) -> System.out.println(i);
         intConsumer.accept(5);
+    }
 
+    public void testIntFunction()
+    {
         IntFunction<Car> intFunction = (int i) -> {
             return new Car(i);
         };
         Car car = intFunction.apply(1);
         System.out.println(car.getCardId());
+    }
 
+    public void testToDoubleFunction()
+    {
         ToDoubleFunction<Car> toDoubleFunction = (Car carTwo) -> carTwo.getCardId();
         double carId = toDoubleFunction.applyAsDouble(new Car(8));
         System.out.println(carId);
+    }
 
-        BooleanSupplier booleanSupplier = ()->{return true;};
+    public void testIntToDoubleFunction(){
+        IntToDoubleFunction intToDouble = (int i) -> {
+            return i * 2;
+        };
+        double testDouble = intToDouble.applyAsDouble(3);
+        System.out.println(testDouble);
+    }
+
+    public void testBooleanSupplier(){
+        BooleanSupplier booleanSupplier = () -> {
+            return true;
+        };
         boolean isTrue = booleanSupplier.getAsBoolean();
         System.out.println(isTrue);
+    }
 
-       ToIntBiFunction<Car,Apple> toIntBiFunction = (Car carOne,Apple apple) ->{
-           int carOneId = carOne.getCardId();
-           int  weight=(int) apple.getWeight();
-          return carOneId+weight;
-       };
-        int toInt = toIntBiFunction.applyAsInt(new Car(888),new Apple("red",77));
+    public void testToIntBiFunction(){
+        ToIntBiFunction<Car, Apple> toIntBiFunction = (Car carOne, Apple apple) -> {
+            int carOneId = carOne.getCardId();
+            int weight = (int)apple.getWeight();
+            return carOneId + weight;
+        };
+        int toInt = toIntBiFunction.applyAsInt(new Car(888), new Apple("red", 77));
         System.out.println(toInt);
-
     }
 }
