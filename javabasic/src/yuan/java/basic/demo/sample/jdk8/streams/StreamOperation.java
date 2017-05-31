736ef13e86680788291751db6e9c20ccc704b586
yuan <yuanxin19900930@icloud.com>
2017-05-31 22:33:09 +0800

    jdk8 chaprter 5

--- javabasic/src/yuan/java/basic/demo/sample/jdk8/streams/StreamOperation.java
+++ javabasic/src/yuan/java/basic/demo/sample/jdk8/streams/StreamOperation.java
@@ -3,46 +3,54 @@ package yuan.java.basic.demo.sample.jdk8.streams;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by yuanxin on 17/5/31.
  */
-public class StreamOperation
-{
+public class StreamOperation {
     private static List<Dish> menu = Arrays.asList(new Dish("pork", false, 1000, Type.MEAT),
-        new Dish("beef", false, 700, Type.MEAT),
-        new Dish("chicken", false, 400, Type.MEAT),
-        new Dish("french fries", true, 530, Type.OTHER),
-        new Dish("rice", true, 350, Type.OTHER),
-        new Dish("season fruit", true, 120, Type.OTHER),
-        new Dish("pizza", true, 500, Type.OTHER),
-        new Dish("prawns", false, 300, Type.FISH),
-        new Dish("salmon", false, 450, Type.FISH)
+            new Dish("beef", false, 700, Type.MEAT),
+            new Dish("chicken", false, 400, Type.MEAT),
+            new Dish("french fries", true, 530, Type.OTHER),
+            new Dish("rice", true, 350, Type.OTHER),
+            new Dish("season fruit", true, 120, Type.OTHER),
+            new Dish("pizza", true, 500, Type.OTHER),
+            new Dish("prawns", false, 300, Type.FISH),
+            new Dish("salmon", false, 450, Type.FISH)
     );
 
-    private void fiterByVegetarian()
-    {
+    private void fiterByVegetarian() {
         menu.stream()
-            .filter(Dish::isVegetarian)
-            .collect(Collectors.toList());
-    }
-    private void distinctTest(){
-        List<Integer> numbers = Arrays.asList(1,2,1,3,3,2,4);
-        numbers.stream().filter(i -> i%2 == 0).distinct().forEach(System.out::println);
-    }
-    private void testSkip(){
-       List<Dish> list =  menu.stream().filter(d -> d.getCalories() >300).skip(2).skip(2).collect(Collectors.toList());
-       list.stream().forEach(System.out::println);
+                .filter(Dish::isVegetarian)
+                .collect(toList());
     }
 
-    private void testMap(){
-        List<String> words = Arrays.asList("Java8","Lambads","In","Action");
-        List<Integer> wordLengths = words.stream().map(String :: length).collect(Collectors.toList());
+    private void distinctTest() {
+        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+        numbers.stream().filter(i -> i % 2 == 0).distinct().forEach(System.out::println);
+    }
+
+    private void testSkip() {
+        List<Dish> list = menu.stream().filter(d -> d.getCalories() > 300).skip(2).skip(2).collect(toList());
+        list.stream().forEach(System.out::println);
+    }
+
+    private void testMap() {
+        List<String> words = Arrays.asList("Java8", "Lambads", "In", "Action");
+        List<Integer> wordLengths = words.stream().map(String::length).collect(toList());
         wordLengths.stream().forEach(System.out::println);
     }
 
-    private void testFlatMap(){
-        List<String> words = Arrays.asList("Java8","Lambads","In","Action");
+    private void testFlatMap() {
+        List<String> words = Arrays.asList("Java8", "Lambads", "In", "Action");
+        List<Stream<String>> streamList = words.stream()
+                .map(word -> word.split(""))
+                .map(Arrays::stream)
+                .distinct()
+                .collect(toList());
 
 
     }
