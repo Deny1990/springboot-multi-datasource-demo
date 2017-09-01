98e68329123871bb987a3a24728650c42c2223cd
yuanxin <yuanxin@shhxzq.com>
2017-09-01 14:24:01 +0800

    mainThreadTest

---
+++ javabasic/src/yuan/java/basic/demo/sample/MainThreadTest.java
@@ -0,0 +1,12 @@
+package yuan.java.basic.demo.sample;
+
+/**
+ * Created by yuanxin on 17/9/1.
+ */
+public class MainThreadTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("currentThread is " +Thread.currentThread().isDaemon());
+    }
+}
