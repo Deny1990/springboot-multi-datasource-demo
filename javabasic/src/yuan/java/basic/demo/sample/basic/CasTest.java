46f4dffd15455585ad2015edc7073befe768a42f
yuanxin <yuanxin@shhxzq.com>
2017-12-13 15:05:04 +0800

    CasTest

---
+++ javabasic/src/yuan/java/basic/demo/sample/basic/CasTest.java
@@ -0,0 +1,56 @@
+package yuan.java.basic.demo.sample.basic;
+
+/**
+ * Created by yuanxin on 17/12/13.
+ */
+public class CasTest
+{
+    private transient volatile int sizeCtl;
+
+//    private static final sun.misc.Unsafe U;
+//
+//    private static final long SIZECTL;
+
+//    static
+//    {
+//        try
+//        {
+//            U = sun.misc.Unsafe.getUnsafe();
+//            Class<?> k = ConcurrentHashMap.class;
+//            SIZECTL = U.objectFieldOffset
+//                (k.getDeclaredField("sizeCtl"));
+//        }
+//        catch (Exception e)
+//        {
+//            throw new Error(e);
+//        }
+//    }
+
+    public void test()
+    {
+        if (true)
+        {
+            try
+            {
+                if (false)
+                {
+                    System.out.printf("hello");
+                }
+
+            }
+            finally
+            {
+                sizeCtl = 4;
+            }
+        }
+        System.out.println(sizeCtl);
+
+    }
+
+    public static void main(String[] args)
+    {
+        CasTest t = new CasTest();
+        t.test();
+    }
+
+}
