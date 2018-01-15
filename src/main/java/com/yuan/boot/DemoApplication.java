7b0d08b94f2e6975e495c9b3cef996c49e99c65e
yuanxin <yuanxin@shhxzq.com>
2018-01-15 13:43:40 +0800

    springboot 使用外部tomcat启动

--- src/main/java/com/yuan/boot/DemoApplication.java
+++ src/main/java/com/yuan/boot/DemoApplication.java
@@ -2,14 +2,25 @@ package com.yuan.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @ServletComponentScan
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer
+{
 
+	/**
+	 * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		builder.sources(this.getClass());
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
