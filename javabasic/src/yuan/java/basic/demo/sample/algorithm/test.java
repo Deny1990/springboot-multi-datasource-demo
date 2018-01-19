46f9c3491e5d6e8c06ad39acdb5db3879dd07f43
yuanxin <yuanxin@shhxzq.com>
2018-01-19 18:41:15 +0800

    最小字符串匹配

--- javabasic/src/yuan/java/basic/demo/sample/algorithm/test.java
+++ javabasic/src/yuan/java/basic/demo/sample/algorithm/test.java
@@ -25,5 +25,52 @@ public class test
         System.out.println(ch3);
         int c = a + b;
         System.out.println(c);
+        int[] targethash = new int[256];
+        minWindow("ADOBECODEBANC","ABC");
+
+
+    }
+
+    static int initTargetHash(int []targethash, String Target) {
+        int targetnum =0 ;
+        for (char ch : Target.toCharArray()) {
+            targetnum++;
+            targethash[ch]++;
+        }
+        return targetnum;
+    }
+    static  boolean valid(int []sourcehash, int []targethash) {
+
+        for(int i = 0; i < 256; i++) {
+            if(targethash[i] > sourcehash[i])
+                return false;
+        }
+        return true;
+    }
+    public static String minWindow(String Source, String Target) {
+        // queueing the position that matches the char in T
+        int ans = Integer.MAX_VALUE;
+        String minStr = "";
+
+        int[] sourcehash = new int[256];
+        int[] targethash = new int[256];
+
+        initTargetHash(targethash, Target);
+        int j = 0, i = 0;
+        for(i = 0; i < Source.length(); i++) {
+            while( !valid(sourcehash, targethash) && j < Source.length()  ) {
+                sourcehash[Source.charAt(j)]++;
+                System.out.println(Source.charAt(j));
+                j++;
+            }
+            if(valid(sourcehash, targethash) ){
+                if(ans > j - i ) {
+                    ans = Math.min(ans, j - i );
+                    minStr = Source.substring(i, j );
+                }
+            }
+            sourcehash[Source.charAt(i)]--;
+        }
+        return minStr;
     }
 }
